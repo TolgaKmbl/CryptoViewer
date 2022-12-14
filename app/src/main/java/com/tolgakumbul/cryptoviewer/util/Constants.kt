e5a8d67a885dc05d146cc1c92ce42f52fd271054
Tolga Kumbul <61317042+TolgaKmbl@users.noreply.github.com>
2022-12-14 12:24:08 +0300

    util classes added, some styles changed

---
+++ app/src/main/java/com/tolgakumbul/cryptoviewer/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.tolgakumbul.cryptoviewer.util
+
+object Constants {
+
+    const val BASE_URL = ""/* Will be added */
+    const val API_KEY = "" /* Will be added */
+    const val CALL_ATTRIBUTES = "" /* Will be added */
+
+}
