46e9dd402be9db45275c934ab23c6ec9893fcf56
Tolga Kumbul <61317042+TolgaKmbl@users.noreply.github.com>
2023-01-14 22:12:20 +0300

    row and list composables

--- app/src/main/java/com/tolgakumbul/cryptoviewer/view/CryptoListScreen.kt
+++ app/src/main/java/com/tolgakumbul/cryptoviewer/view/CryptoListScreen.kt
@@ -1,13 +1,15 @@
 package com.tolgakumbul.cryptoviewer.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.onFocusChanged
@@ -19,6 +21,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.tolgakumbul.cryptoviewer.model.CryptoListItem
 import com.tolgakumbul.cryptoviewer.viewmodel.CryptoListViewModel
 
 @Composable
@@ -50,10 +53,11 @@ fun CryptoListScreen(
                     .fillMaxWidth()
                     .padding(16.dp)
             ) {
-                //viewModel.searchCryptoList(it)
+                viewModel.searchCryptoList(it)
             }
 
             Spacer(modifier = Modifier.height(10.dp))
+            CryptoList(navController = navController)
         }
 
     }
@@ -104,4 +108,81 @@ fun SearchBar(
     }
 
 
+}
+
+@Composable
+fun CryptoList(navController: NavController, viewModel: CryptoListViewModel = hiltViewModel()) {
+    val cryptoList by remember {
+        viewModel.cryptoList
+    }
+    val errorMessage by remember {
+        viewModel.errorMessage
+    }
+    val isLoading by remember {
+        viewModel.isLoading
+    }
+    CryptoListView(cryptoList, navController)
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+        if (isLoading) {
+            CircularProgressIndicator(color = MaterialTheme.colors.primary)
+        }
+        if (errorMessage.isNotEmpty()) {
+            RetryView(errorMsg = errorMessage) {
+                viewModel.loadCryptos()
+            }
+        }
+    }
+}
+
+@Composable
+fun CryptoListView(cryptos: List<CryptoListItem>, navController: NavController) {
+    LazyColumn(contentPadding = PaddingValues(5.dp)) {
+        items(cryptos) { crypto ->
+            CryptoRow(navController, crypto)
+        }
+    }
+
+}
+
+@Composable
+fun CryptoRow(navController: NavController, crypto: CryptoListItem) {
+
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .background(color = MaterialTheme.colors.secondary)
+        .clickable {
+            navController.navigate(
+                "crypto_detail_screen/${crypto.currency}/${crypto.price}"
+            )
+        }
+    ) {
+        Text(
+            text = crypto.currency,
+            style = MaterialTheme.typography.h4,
+            modifier = Modifier.padding(2.dp),
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colors.primary
+        )
+        Text(
+            text = crypto.price,
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier.padding(2.dp),
+            color = MaterialTheme.colors.primaryVariant
+        )
+
+    }
+
+}
+
+@Composable
+fun RetryView(errorMsg: String, onRetry: () -> Unit) {
+    Column() {
+        Text(text = errorMsg, color = Color.Red, fontSize = 20.sp)
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(onClick = {
+            onRetry
+        }, modifier = Modifier.align(Alignment.CenterHorizontally)) {
+            Text(text = "Retry")
+        }
+    }
 }
